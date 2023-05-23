b401a15d77c819c30495662c3d61c1889653c647
MiguelAnaya <mianare.99.anaya@gmail.com>
2023-05-23 14:42:13 -0700

    html succesfully converted to php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require_once("./inc/Page.class.php");
+
+
+
+
+
+
+
+// html page
+echo Page::htmlHeader();
+
+echo Page::header();
+echo Page::about();
+echo Page::truckList();
+echo Page::reviews();
+echo Page::container();
+echo Page::map();
+echo Page::footer();
+
+echo Page::htmlFooter();

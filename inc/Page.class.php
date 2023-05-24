6809e2d1d4929dc8a4637b216f95d26238520f22
MiguelAnaya <mianare.99.anaya@gmail.com>
2023-05-24 13:46:56 -0700

    trucks section layout done

--- inc/Page.class.php
+++ inc/Page.class.php
@@ -10,7 +10,7 @@ class Page{
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="./scss/style.scss">
+            <link rel="stylesheet" href="./css/style.css">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
             <title>final-project</title>
         </head>
@@ -52,8 +52,22 @@ class Page{
 
     public static function truckList(){
         $truckList = '
-        <div class="menu" id="trucks">
-            <h2>Menu</h2>
+        <div id="trucks" class="trucks">
+            <a href="#">
+                <div class="brazil">
+                    Brazilian
+                </div>
+            </a>
+            <a href="#">
+                <div class="colombia">
+                    Colombian
+                </div>
+            </a>
+            <a href="#">
+                <div class="japan">
+                    Japanese
+                </div>
+            </a>
         </div>';
         return $truckList;
     }
